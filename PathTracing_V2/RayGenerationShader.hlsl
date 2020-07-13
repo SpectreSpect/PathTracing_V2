961cf8b541162f4a1fece96cb60bb9323ccfd628
SpectreSpect <EmailGax8@Gmail.com>
2020-07-13 19:35:44 +0400

    Скрины

--- PathTracing_V2/RayGenerationShader.hlsl
+++ PathTracing_V2/RayGenerationShader.hlsl
@@ -18,6 +18,6 @@ void main(uint3 threadID : SV_DispatchThreadID)
 {
 	uint width, height;
 	Result.GetDimensions(width, height);
-	float2 uv = float2(((threadID.x / width) * 2) - 1)
+	//float2 uv = float2(((threadID.x / width) * 2) - 1)
 
 }
